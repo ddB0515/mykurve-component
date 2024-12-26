639598aba8dc23114cb6ca353d1a8b88d7a45c26
ddB0515 <dario.budimir@gmail.com>
2024-12-26 13:42:11 +0000

    initial working sensor

--- custom_components/mykurve_energy/sensor.py
+++ custom_components/mykurve_energy/sensor.py
@@ -1,58 +1,90 @@
 """Support for MyKurve Energy Pricing data"""
 
-import asyncio
-from datetime import timedelta
+from __future__ import annotations
+from datetime import timedelta, datetime
 import logging
-from typing import Any
+import asyncio
+from dataclasses import dataclass
+from collections.abc import Callable
 
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorEntity, SensorEntityDescription, SensorDeviceClass, SensorStateClass
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_API_TOKEN, UnitOfEnergy
+from homeassistant.const import UnitOfEnergy, CONF_API_TOKEN
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from mykurve import MyKurveApi
+from mykurve.data_classes import Dashboard
 
 from . import MyKurveAuth
 from .const import DOMAIN, CONF_ACC_NUMBER
 
 _LOGGER = logging.getLogger(__name__)
 
-SCAN_INTERVAL = timedelta(minutes=2)
+SCAN_INTERVAL = timedelta(minutes=5)
+
+@dataclass(frozen=True, kw_only=True)
+class MyKurveSensorEntityDescription(SensorEntityDescription):
+    """MyKurve Energy sensor description."""
+    value_fn: Callable[[Dashboard], StateType | datetime]
+
+ENERGY_METER_SENSOR_TYPES = (
+    MyKurveSensorEntityDescription(
+        key="usage_kwh",
+        icon="mdi:fire",
+        name="Energy usage kWh",
+        native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
+        device_class=SensorDeviceClass.ENERGY,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        value_fn=lambda res: res.lastMeterReading,
+    ),
+    MyKurveSensorEntityDescription(
+        key="price_gbp",
+        icon="mdi:cash-multiple",
+        name="Energy price usage",
+        device_class=SensorDeviceClass.MONETARY,
+        native_unit_of_measurement="GBP",
+        suggested_display_precision=3,
+        value_fn=lambda res: res.tariffHistory.tariffInForceNow.rate,
+    ),
+)
+
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """MyKurve Energy Sensor Setup."""
+    """MyKurve Energy Sensor Setup"""
     myKurveAuth: MyKurveAuth = hass.data[DOMAIN][entry.entry_id]
 
-    async_add_entities([MyKurveSensor(myKurveAuth, entry)], True)
-
+    async_add_entities(
+        (MyKurveSensor(myKurveAuth, entry, description)
+        for description in ENERGY_METER_SENSOR_TYPES),
+        True
+    )
 
 class MyKurveSensor(SensorEntity):
     """Entity object for MyKurve Energy sensor"""
 
-    _attr_attribution = "Data provided by MyKurve Energy"
-    _attr_native_unit_of_measurement = f"£/{UnitOfEnergy.KILO_WATT_HOUR}"
-    _attr_has_entity_name = True
-    _attr_translation_key = "power_price"
-    _attributes: dict[str, Any] = {}
+    entity_description: MyKurveSensorEntityDescription
 
-    def __init__(self, mykurveauth: MyKurveAuth, entry: ConfigEntry) -> None:
-        """Entity object for MyKurve Energy sensor"""
-        self._api = MyKurveApi()
-        self._mykurve_auth = mykurveauth
-        self._entry = entry
-        self._price = 0.0
+    def __init__(
+        self,
+        mykurveauth: MyKurveAuth,
+        entry: ConfigEntry,
+        description: MyKurveSensorEntityDescription,
+    ) -> None:
+        self.dashboard = None
+        self.mykurve_auth = mykurveauth
+        self.api = MyKurveApi()
+        self.entry = entry
 
-    @property
-    def native_value(self):
-        """Return the state of the sensor"""
-        return self._price
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes"""
-        return self._attributes
+        """Set up the sensor with the initial values."""
+        self.key = description.key
+        self._attr_unique_id = (
+            f"{DOMAIN}_{description.key}"  # type: ignore[union-attr]
+        )
+        self._attr_name = description.name
+        self.entity_description = description
 
     async def async_update(self) -> None:
         """Get the MyKurve Energy data from the web service"""
@@ -60,11 +92,13 @@ class MyKurveSensor(SensorEntity):
         #     return  # Power price data is still valid
 
         async with asyncio.timeout(30):
-            await self._mykurve_auth.async_get_access_token()
-            token = self._entry.data[CONF_API_TOKEN]
-            acc_number = self._entry.data[CONF_ACC_NUMBER]
-            _LOGGER.debug("token: %s - acc_no", token, acc_number)
-            dashboard = await self._api.get_dashboard(token, acc_number)
-            self._price = dashboard.tariffHistory.tariffInForceNow.rate
+            await self.mykurve_auth.async_get_access_token()
+            token = self.entry.data[CONF_API_TOKEN]
+            acc_number = self.entry.data[CONF_ACC_NUMBER]
+            _LOGGER.debug(f"token: {token} - acc_no: {acc_number}")
+            self.dashboard = await self.api.get_dashboard(token, acc_number)
 
-        _LOGGER.debug("Pricing data: %s", self._price)
+    @property
+    def native_value(self) -> StateType | datetime:
+        """Return the state of the sensor."""
+        return self.entity_description.value_fn(self.dashboard)
